f0249a1a03625eb7ef2c5be4e686eb1cf214e31a
Shaan Chauhan <shaanbroncos@gmail.com>
2023-04-16 22:44:05 -0600

    Update MNIST Neural Network.py

--- Projects/MNIST Neural Network.py
+++ Projects/MNIST Neural Network.py
@@ -9,9 +9,9 @@ class Network():
         self.hiddenSize = hiddenSize
         self.outputSize = outputSize
     #Set all weights and biases
-        self.W1 = np.random.randn(self.inputSize, self.hiddenSize) #2d array with dimension (input x hidden)
-        self.b1 = np.zeros((1, self.hiddenSize)) #2D array with dimension (1 x hidden size)
-        self.W2 = np.random.randn(self.hiddenSize, self.outputSize) #2d array with dimension (hidden x Output)
-        self.b2 = np.zeros((1, self.outputSize)) #2D array with dimension (1 x output size)
+        self.W1 = np.random.randn(self.inputSize, self.hiddenSize) #2d array with dimension (input rows x hidden columns)
+        self.b1 = np.zeros((1, self.hiddenSize)) #2D array with dimension (1 row x hidden column)
+        self.W2 = np.random.randn(self.hiddenSize, self.outputSize) #2d array with dimension (hidden rows x Output columns)
+        self.b2 = np.zeros((1, self.outputSize)) #2D array with dimension (1 row x output column)
 
 
