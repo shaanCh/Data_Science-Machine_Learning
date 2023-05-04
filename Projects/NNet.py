9d41c0653db4c6a2bae621675f37377d32c747d4
Shaan Chauhan <shaanbroncos@gmail.com>
2023-05-04 09:43:33 -0600

    Create NNet.py

---
+++ Projects/NNet.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+# Define the transformation to apply to the input data
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# Load the MNIST dataset and apply the transformation
+train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST('./data', train=False, download=True, transform=transform)
+
+# Create data loaders for batch processing
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+# Define the model architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(784, 64)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return torch.log_softmax(x, dim=1)
+
+# Create an instance of the model
+model = Net()
+
+# Define the loss function and optimizer
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Train the model
+for epoch in range(10):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        inputs = inputs.view(-1, 784)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+# Evaluate the model on test data
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs = inputs.view(-1, 784)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
